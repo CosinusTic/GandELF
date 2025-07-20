9d1782b987068f8e0c8b30025fdc9b3f52b3a6a0
Nathan <delmarchenathan@gmail.com>
2025-07-20 21:46:28 +0200

    FEAT: pretty print target system

--- src/pretty_print.c
+++ src/pretty_print.c
@@ -3,16 +3,59 @@
 #include <elf.h>
 #include <stdio.h>
 
+static void print_target_sys(Elf64_Ehdr *hdr)
+{
+    printf("Target system:\t");
+    switch (hdr->e_ident[EI_OSABI])
+    {
+    case ELFOSABI_NETBSD:
+        printf("Net BSD");
+        break;
+    case ELFOSABI_FREEBSD:
+        printf("Free BSD");
+        break;
+    case ELFOSABI_LINUX:
+        printf("Linux");
+        break;
+    case ELFOSABI_SYSV:
+        printf("Unix System V");
+        break;
+    case ELFOSABI_SOLARIS:
+        printf("Solaris");
+        break;
+    case ELFOSABI_ARM:
+        printf("ARM architecture");
+        break;
+    case ELFOSABI_STANDALONE:
+        printf("Standalone executable (embedded)");
+        break;
+    default:
+        printf("?");
+        break;
+    }
+    printf("\n");
+}
+
+static void print_arch(Elf64_Ehdr *hdr)
+{
+    printf("Architecture:\t");
+    switch (hdr->e_ident[EI_CLASS])
+    {
+    case ELFCLASS64:
+        printf("x86_64");
+        break;
+    case ELFCLASS32:
+        printf("x86 (32 bits)");
+        break;
+    default:
+        printf("?");
+    }
+    printf("\n");
+}
+
 void print_headers(const struct file *f)
 {
-    Elf64_Ehdr *fheaders = (Elf64_Ehdr *)f->content;
-
-    printf("Architecture:\t");
-    if (fheaders->e_ident[EI_CLASS] == ELFCLASS64)
-        printf("x86_64");
-    else if (fheaders->e_ident[EI_CLASS] == ELFCLASS32)
-        printf("x86 (32 bits)");
-    else
-        printf("Unknown");
-    printf("\n");
+    Elf64_Ehdr *hdr = (Elf64_Ehdr *)f->content;
+    print_arch(hdr);
+    print_target_sys(hdr);
 }
